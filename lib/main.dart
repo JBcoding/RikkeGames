997522e48e0583d462cd059726e3b3f1648ab8bf
Mads Bjørn <mads12bjoern@gmail.com>
2022-01-09 19:03:53 +0100

    Generalized NumberPairGame and added twin version

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:jiggle/controller.dart';
-import 'package:jiggle/jiggle.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
+import 'number_pair_game.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,152 +13,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Matematik - 10'er venner - Et spil af Rikke Bjørn",
+      title: "Matematik - Spil af Rikke Bjørn",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const NumberPairGame(title: "Matematik - 10'er venner - Et spil af Rikke Bjørn"),
+      home: const MainMenu(title: "Matematik - Spil af Rikke Bjørn"),
     );
   }
 }
 
-class NumberPairGame extends StatefulWidget {
-  const NumberPairGame({Key? key, required this.title}) : super(key: key);
+class MainMenu extends StatefulWidget {
+  const MainMenu({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<NumberPairGame> createState() => _NumberPairGameState();
+  State<MainMenu> createState() => _MainMenuState();
 }
 
-class NumberTwinsBoardElement {
-  int value = 0;
-  bool used = false;
-  final JiggleController jiggleControllerCompletionAnimation = JiggleController();
-  final JiggleController jiggleControllerErrorAnimation = JiggleController();
-
-  NumberTwinsBoardElement(this.value);
-
-  Color getColor(NumberTwinsBoardElement? selectedElement) {
-    if (this == selectedElement) {
-      return const Color.fromRGBO(0, 0, 110, 1);
-    } else if (used) {
-      return const Color.fromRGBO(0, 180, 0, 1);
-    } else if (jiggleControllerErrorAnimation.isJiggling) {
-      return const Color.fromRGBO(210, 0, 0, 1);
-    }
-    return const Color.fromRGBO(0, 0, 220, 1);
-  }
-}
-
-class _NumberPairGameState extends State<NumberPairGame> {
-  List<List<NumberTwinsBoardElement>> board = List.empty();
-  NumberTwinsBoardElement? selectedElement;
-  int points = 0;
-  int lastPairTime = 0;
-  SharedPreferences? prefs;
-
-  _NumberPairGameState() {
-    _setupPrefs();
-    _setupGame();
-  }
-
-  Future<void> _setupPrefs() async {
-    SharedPreferences newPrefs = await SharedPreferences.getInstance();
-    setState(() {
-      prefs = newPrefs;
-    });
-  }
-
-  void _setupGame() {
-    List<int> numbers = List.generate(6 * 3, (index) => Random().nextInt(9) + 1);
-    numbers.addAll(List.generate(numbers.length, (index) => 10 - numbers[index]));
-    numbers.shuffle();
-    board = List.generate(6, (i) => List.generate(6, (j) => NumberTwinsBoardElement(numbers[i * 6 + j])));
-    selectedElement = null;
-    points = 0;
-    lastPairTime = DateTime.now().millisecondsSinceEpoch;
-  }
-
-  void _reload() {
-    setState(() {
-      _setupGame();
-    });
-  }
-
-  int _calculatePoints() {
-    int millisSince = DateTime.now().millisecondsSinceEpoch - lastPairTime;
-    if (millisSince < 3000) {
-      return 5;
-    } else if (millisSince < 5000) {
-      return 4;
-    } else if (millisSince < 10000) {
-      return 2;
-    } else if (millisSince < 20000) {
-      return 2;
-    }
-    return 1;
-  }
-
-  NumberTwinsBoardElement _getRandomBoardElement() {
-    return board[Random().nextInt(board.length)][Random().nextInt(board[0].length)];
-  }
-
-  Future<void> _turnOffToggleInTheFuture(NumberTwinsBoardElement element) async {
-    await Future.delayed(const Duration(seconds: 5));
-    element.jiggleControllerCompletionAnimation.toggle();
-  }
-
-  Future<void> _playCompletionAnimation() async {
-    while (_isDone()) {
-      NumberTwinsBoardElement element = _getRandomBoardElement();
-      while (element.jiggleControllerCompletionAnimation.isJiggling) {
-        element = _getRandomBoardElement();
-      }
-      element.jiggleControllerCompletionAnimation.toggle();
-      _turnOffToggleInTheFuture(element);
-      await Future.delayed(const Duration(milliseconds: 500));
-    }
-  }
-
-  bool _isDone() {
-    return board.every((column) => column.every((element) => element.used));
-  }
-
-  Future<void> _validMove(NumberTwinsBoardElement element) async {
-    setState(() {
-      selectedElement?.used = true;
-      element.used = true;
-      selectedElement = null;
-      points += _calculatePoints();
-      lastPairTime = DateTime.now().millisecondsSinceEpoch;
-    });
-    if (_isDone()) {
-      if (prefs != null) {
-        int highScore = (prefs!.getInt('number_10_high_score') ?? 0);
-        highScore = max(points, highScore);
-        await prefs!.setInt('number_10_high_score', highScore);
-      }
-      _playCompletionAnimation();
-    }
-  }
-
-  Future<void> _invalidMove(NumberTwinsBoardElement element) async {
-    setState(() {
-      element.jiggleControllerErrorAnimation.toggle();
-      selectedElement!.jiggleControllerErrorAnimation.toggle();
-    });
-    points -= 1;
-    points = max(points, 0);
-    NumberTwinsBoardElement? oldSelectedElement = selectedElement;
-    selectedElement = null;
-    await Future.delayed(const Duration(seconds: 1));
-    setState(() {
-      element.jiggleControllerErrorAnimation.toggle();
-      oldSelectedElement!.jiggleControllerErrorAnimation.toggle();
-    });
-  }
-
+class _MainMenuState extends State<MainMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -173,74 +43,47 @@ class _NumberPairGameState extends State<NumberPairGame> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              "Find 10'er venner! Kan du slå din rekord?",
+              "Vælg det spil du vil spille!",
               style: TextStyle(fontSize: 30),
             ),
             Container(
               height: 40,
             ),
-            Text(
-              'Points: $points   (bedste: ${prefs?.getInt('number_10_high_score') ?? 0})',
-              style: const TextStyle(fontSize: 30),
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: const Color.fromRGBO(25, 103, 210, 1),
+                ),
+                child: const Text("10'er venner!", style: TextStyle(fontSize: 44),),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NumberPairGame(type: NumberPairGameTypes.tenGame,)),
+                  );
+                },
+              ),
             ),
             Container(
-              height: 40,
+              height: 5,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: board.map((column) => Column(
-                children: column.map((element) =>
-                    Jiggle(
-                      jiggleController: element.jiggleControllerCompletionAnimation,
-                      extent: 360,
-                      duration: const Duration(milliseconds: 5000),
-                      child: Jiggle(
-                        jiggleController: element.jiggleControllerErrorAnimation,
-                        extent: 25,
-                        duration: const Duration(milliseconds: 300),
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: FittedBox(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: element.getColor(selectedElement),
-                              ),
-                              child: Text('${element.value}', style: const TextStyle(fontSize: 44),),
-                              onPressed: () async {
-                                if (element.used) {
-                                  return;
-                                }
-                                if (selectedElement == element) {
-                                  setState(() {
-                                    selectedElement = null;
-                                  });
-                                } else if (selectedElement == null) {
-                                  setState(() {
-                                    selectedElement = element;
-                                  });
-                                } else {
-                                  if (element.value + selectedElement!.value == 10) {
-                                    _validMove(element);
-                                  } else {
-                                    _invalidMove(element);
-                                  }
-                                }
-                              },
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                ).toList(),
-              )).toList(),
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: const Color.fromRGBO(25, 103, 210, 1),
+                ),
+                child: const Text("Tvillinger!", style: TextStyle(fontSize: 44),),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NumberPairGame(type: NumberPairGameTypes.twinGame,)),
+                  );
+                },
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _reload,
-        tooltip: 'Prøv igen',
-        child: const Icon(Icons.restart_alt),
       ),
     );
   }
---
+++ lib/number_pair_game.dart
@@ -0,0 +1,270 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:jiggle/jiggle.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum NumberPairGameTypes {
+  tenGame,
+  twinGame,
+}
+
+
+class NumberPairGame extends StatefulWidget {
+  const NumberPairGame({Key? key, required this.type}) : super(key: key);
+
+  final NumberPairGameTypes type;
+
+  @override
+  State<NumberPairGame> createState() => _NumberPairGameState(this.type);
+}
+
+class NumberTwinsBoardElement {
+  int value = 0;
+  String showValue = "";
+  bool used = false;
+  final JiggleController jiggleControllerCompletionAnimation = JiggleController();
+  final JiggleController jiggleControllerErrorAnimation = JiggleController();
+
+  NumberTwinsBoardElement(this.value, this.showValue);
+
+  Color getColor(NumberTwinsBoardElement? selectedElement) {
+    if (this == selectedElement) {
+      return const Color.fromRGBO(0, 0, 110, 1);
+    } else if (used) {
+      return const Color.fromRGBO(0, 180, 0, 1);
+    } else if (jiggleControllerErrorAnimation.isJiggling) {
+      return const Color.fromRGBO(210, 0, 0, 1);
+    }
+    return const Color.fromRGBO(0, 0, 220, 1);
+  }
+}
+
+class _NumberPairGameState extends State<NumberPairGame> {
+  List<List<NumberTwinsBoardElement>> board = List.empty();
+  NumberTwinsBoardElement? selectedElement;
+  int points = 0;
+  int lastPairTime = 0;
+  SharedPreferences? prefs;
+
+  late String title;
+  late String description;
+  late String highScoreKey;
+  late double pieceWidth;
+
+  _NumberPairGameState(NumberPairGameTypes type) {
+    if (type == NumberPairGameTypes.tenGame) {
+      title = "Matematik - 10'er venner - Et spil af Rikke Bjørn";
+      description = "Find 10'er venner! Kan du slå din rekord?";
+      highScoreKey = "number_10_high_score";
+      pieceWidth = 55;
+    } else {
+      title = "Matematik - Tvillinger - Et spil af Rikke Bjørn";
+      description = "Find tvillinger! Kan du slå din rekord?";
+      highScoreKey = "number_twin_high_score";
+      pieceWidth = 130;
+    }
+    _setupPrefs();
+    _setupGame(type);
+  }
+
+  Future<void> _setupPrefs() async {
+    SharedPreferences newPrefs = await SharedPreferences.getInstance();
+    setState(() {
+      prefs = newPrefs;
+    });
+  }
+
+  String _getTwinGameShowString(int value) {
+    if (value > 0) {
+      return (value - 100).toString() + " + " + (value - 100).toString();
+    } else {
+      return "= " + ((-value + 10 - 100) * 2).toString();
+    }
+  }
+
+  void _setupGame(NumberPairGameTypes type) {
+    if (type == NumberPairGameTypes.tenGame) {
+      List<int> numbers = List.generate(6 * 3, (index) => Random().nextInt(9) + 1);
+      numbers.addAll(List.generate(numbers.length, (index) => 10 - numbers[index]));
+      numbers.shuffle();
+      board = List.generate(6, (i) =>
+          List.generate(6, (j) => NumberTwinsBoardElement(numbers[i * 6 + j], numbers[i * 6 + j].toString())));
+    } else {
+      List<int> numbers = List.generate(6 * 3, (index) => Random().nextInt(9) + 1 + 100);
+      numbers.addAll(List.generate(numbers.length, (index) => - numbers[index] + 10));
+      numbers.shuffle();
+      board = List.generate(6, (i) =>
+          List.generate(6, (j) => NumberTwinsBoardElement(numbers[i * 6 + j], _getTwinGameShowString(numbers[i * 6 + j]))));
+    }
+    selectedElement = null;
+    points = 0;
+    lastPairTime = DateTime.now().millisecondsSinceEpoch;
+  }
+
+  void _reload() {
+    setState(() {
+      _setupGame(widget.type);
+    });
+  }
+
+  int _calculatePoints() {
+    int millisSince = DateTime.now().millisecondsSinceEpoch - lastPairTime;
+    if (millisSince < 3000) {
+      return 5;
+    } else if (millisSince < 5000) {
+      return 4;
+    } else if (millisSince < 10000) {
+      return 2;
+    } else if (millisSince < 20000) {
+      return 2;
+    }
+    return 1;
+  }
+
+  NumberTwinsBoardElement _getRandomBoardElement() {
+    return board[Random().nextInt(board.length)][Random().nextInt(board[0].length)];
+  }
+
+  Future<void> _turnOffToggleInTheFuture(NumberTwinsBoardElement element) async {
+    await Future.delayed(const Duration(seconds: 5));
+    element.jiggleControllerCompletionAnimation.toggle();
+  }
+
+  Future<void> _playCompletionAnimation() async {
+    while (_isDone()) {
+      NumberTwinsBoardElement element = _getRandomBoardElement();
+      while (element.jiggleControllerCompletionAnimation.isJiggling) {
+        element = _getRandomBoardElement();
+      }
+      element.jiggleControllerCompletionAnimation.toggle();
+      _turnOffToggleInTheFuture(element);
+      await Future.delayed(const Duration(milliseconds: 500));
+    }
+  }
+
+  bool _isDone() {
+    return board.every((column) => column.every((element) => element.used));
+  }
+
+  Future<void> _validMove(NumberTwinsBoardElement element) async {
+    setState(() {
+      selectedElement?.used = true;
+      element.used = true;
+      selectedElement = null;
+      points += _calculatePoints();
+      lastPairTime = DateTime.now().millisecondsSinceEpoch;
+    });
+    if (_isDone()) {
+      if (prefs != null) {
+        int highScore = (prefs!.getInt(highScoreKey) ?? 0);
+        highScore = max(points, highScore);
+        await prefs!.setInt(highScoreKey, highScore);
+      }
+      _playCompletionAnimation();
+    }
+  }
+
+  Future<void> _invalidMove(NumberTwinsBoardElement element) async {
+    setState(() {
+      element.jiggleControllerErrorAnimation.toggle();
+      selectedElement!.jiggleControllerErrorAnimation.toggle();
+    });
+    points -= 1;
+    points = max(points, 0);
+    NumberTwinsBoardElement? oldSelectedElement = selectedElement;
+    selectedElement = null;
+    await Future.delayed(const Duration(seconds: 1));
+    setState(() {
+      element.jiggleControllerErrorAnimation.toggle();
+      oldSelectedElement!.jiggleControllerErrorAnimation.toggle();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              description,
+              style: const TextStyle(fontSize: 30),
+            ),
+            Container(
+              height: 40,
+            ),
+            Text(
+              'Points: $points   (bedste: ${prefs?.getInt(highScoreKey) ?? 0})',
+              style: const TextStyle(fontSize: 30),
+            ),
+            Container(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: board.map((column) => Column(
+                children: column.map((element) =>
+                    Jiggle(
+                      jiggleController: element.jiggleControllerCompletionAnimation,
+                      extent: 360,
+                      duration: const Duration(milliseconds: 5000),
+                      child: Jiggle(
+                        jiggleController: element.jiggleControllerErrorAnimation,
+                        extent: 25,
+                        duration: const Duration(milliseconds: 300),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: FittedBox(
+                            child: SizedBox(
+                              width: pieceWidth,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: element.getColor(selectedElement),
+                                ),
+                                child: Text(element.showValue, style: const TextStyle(fontSize: 44),),
+                                onPressed: () async {
+                                  if (element.used) {
+                                    return;
+                                  }
+                                  if (selectedElement == element) {
+                                    setState(() {
+                                      selectedElement = null;
+                                    });
+                                  } else if (selectedElement == null) {
+                                    setState(() {
+                                      selectedElement = element;
+                                    });
+                                  } else {
+                                    if (element.value + selectedElement!.value == 10) {
+                                      _validMove(element);
+                                    } else {
+                                      _invalidMove(element);
+                                    }
+                                  }
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                ).toList(),
+              )).toList(),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _reload,
+        tooltip: 'Prøv igen',
+        child: const Icon(Icons.restart_alt),
+      ),
+    );
+  }
+}
